40018e5446c701bb38c89feb640f158af942ee3e
Pavel <razor8ne8@icloud.com>
2021-11-26 18:44:17 +0200

    Add check list and some tests to the project.

--- src/test/java/HeaderPageTest.java
+++ src/test/java/HeaderPageTest.java
@@ -1,13 +1,14 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.MainPage;
+import utils.PropertyReader;
 
 public class HeaderPageTest extends BaseTest {
 
     @Test(description = "Checking the possibility of exiting from account")
     public void logOutTest() {
         loginPage.openPage()
-                .login("pablo777@mailinator.com", "TestPass123")
+                .login(System.getProperty("mail", PropertyReader.getProperty("mail")), System.getProperty("password", PropertyReader.getProperty("password")))
                 .logout();
         Assert.assertTrue(MainPage.isImageOnMainPageDisplayed());
     }
